cbd7c3c4440ec9a0aa7d1ad4f62954149a2bc153
Gabriel <gabrielcemp@gmail.com>
2021-09-30 16:20:27 -0300

    feat: add update entry

---
+++ functions/src/controllers/UpdateEntryController.ts
@@ -0,0 +1,26 @@
+import {Request, Response} from "express";
+import {db} from "../config/firebase";
+
+export class UpdateEntryController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const {body: {title, text}, params: {entryId}} = req;
+
+    const entry = db.collection("entries").doc(entryId);
+    const currentEntryData = (await entry.get()).data() || {};
+
+    const entryObject = {
+      title: title || currentEntryData.title,
+      text: text || currentEntryData.text,
+    };
+
+    await entry.set(entryObject).catch((err) => {
+      throw new Error(err.message);
+    });
+
+    return res.json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject,
+    });
+  }
+}
--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -1,13 +1,16 @@
 import {Router} from "express";
 import {AddEntryController} from "./controllers/AddEntryController";
 import {GetAllEntriesController} from "./controllers/GetAllEntriesController";
+import {UpdateEntryController} from "./controllers/UpdateEntryController";
 
 const router = Router();
 
 const addEntryController = new AddEntryController();
 const getAllEntriesController = new GetAllEntriesController();
+const updateEntryController = new UpdateEntryController();
 
 router.post("/entry", addEntryController.handle);
 router.get("/entry", getAllEntriesController.handle);
+router.put("/entry/:entryId", updateEntryController.handle);
 
 export {router};
