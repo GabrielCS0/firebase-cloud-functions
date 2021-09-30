6f574a319b3c7a04382e9fd9f6d61dc4a1a54623
Gabriel <gabrielcemp@gmail.com>
2021-09-30 10:35:54 -0300

    add express & firebase config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/", (_, res) => res.json({message: "Success"}));
+
+exports.app = functions.https.onRequest(app);
